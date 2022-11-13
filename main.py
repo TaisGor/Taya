cc1adda29416158217b8068a7b52be4632558611
dell2 <gorbacht135@gmail.com>
2022-11-13 11:06:49 +0100

    Vertion 0.1

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+
+
+squares = (e ** 2 for e in range(0, 11, 2))
+
+
+def squares2():
+    print('Generator working...')
+    for e in range(0, 11, 2):
+        yield e ** 2
+
+
+def pause():
+    print('Generator working...')
+    wile True:
+    print(a)
+
+
+
+a = 10
+gen = pause()
+print(next(gen))
+a = 20
+print(next(gen))
+a = 100500
+print(next(gen))
